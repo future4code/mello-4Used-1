39e87863700326ea331791548b35f0c960b41178
thiago stephen <thiagosop@yahoo.com>
2020-06-10 12:30:24 -0300

    desenvolvido o componente carrinho

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { GridCarrinho } from './GridCarrinho'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
+        <GridCarrinho />
 
       </div>
     )
