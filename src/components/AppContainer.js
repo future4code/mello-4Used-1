dec1f4c2b8abfe6e010493794a0f45854f6b29ee
Luiz <luizmitsuru@gmail.com>
2020-06-10 11:02:15 -0300

    Grid adicionado

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Produtos from './Produtos'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
+        <Produtos />
+
 
       </div>
     )
