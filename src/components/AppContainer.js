513cd5ba066cdd001d048b9f052b72a204c628eb
thiago stephen <thiagosop@yahoo.com>
2020-06-10 21:00:12 -0300

    desenvolvi a pagina de cadastro de produto

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { GridCarrinho } from './GridCarrinho'
+import {CadastrarProduto} from './PaginaCadastroProodutos'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-        <GridCarrinho />
+        <CadastrarProduto />
 
       </div>
     )
